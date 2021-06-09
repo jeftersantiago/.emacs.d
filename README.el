baa7f7cba61c1e8ca879b8c81f4f5519f65b7d9f
jefter66 <jeftersantiago@protonmail.com>
2021-06-09 15:05:59 -0300

    switching notebook

--- README.el
+++ README.el
@@ -1,17 +1,17 @@
 (setq-default user-full-name "Jefter Santiago")
-(setq-default user-mail-address "jefterrsantiago@gmail.com")
+(setq-default user-mail-address "jeftersantiago@protonmail.com")
 
 (load "~/.local/bin/private.el")
 
 (setq confirm-kill-processes nil)
- (setq-default transient-mark-mode t)
- (setq-default visual-line-mode t)
- (setq-default truncate-lines nil)
- (setq-default cursor-type 'box)
- (setq-default fill-column 78)
- (setq-default sentence-end-double-space nil)
- ; i don't like syntax highlight very much
- (global-font-lock-mode 0)
+   (setq-default transient-mark-mode t)
+   (setq-default visual-line-mode t)
+   (setq-default truncate-lines nil)
+   (setq-default cursor-type 'box)
+   (setq-default fill-column 78)
+   (setq-default sentence-end-double-space nil)
+   ; i don't like syntax highlight very much
+;   (global-font-lock-mode 0)
 
 ;;      (fset 'yes-or-no-p 'y-or-n-p)
         (defalias 'yes-or-no-p 'y-or-n-p)
@@ -50,14 +50,18 @@ mouse-wheel-follow-mouse 't)
 
 (setq custom-file "~/.emacs.d/custom.el")
 
-(use-package dracula-theme
-  :config
-  (load-theme 'dracula t)
-  :ensure t)
-(use-package almost-mono-themes
-  :config
-  (load-theme 'almost-mono-black t)
-  :ensure t)
+;    (use-package dracula-theme 
+;      :config
+;      (load-theme 'dracula t)
+;      :ensure t)
+     (use-package cherry-blossom-theme 
+       :config
+       (load-theme 'cherry-blossom t)
+       :ensure t)
+
+;; set transparency
+(set-frame-parameter (selected-frame) 'alpha '(90 90))
+(add-to-list 'default-frame-alist '(alpha 90 90))
 
 (add-to-list 'default-frame-alist '(font . "Monospace 20"))
 ;; https://emacs.stackexchange.com/q/45895
@@ -83,8 +87,8 @@ mouse-wheel-follow-mouse 't)
       (add-hook 'prog-mode-hook 'turn-on-diff-hl-mode)
       (add-hook 'vc-dir-mode-hook 'turn-on-diff-hl-mode))
 
-;(global-display-line-numbers-mode)
-;(setq display-line-numbers-type 'relative)
+(global-display-line-numbers-mode)
+(setq display-line-numbers-type 'relative)
 
 (scroll-bar-mode -1)
 (tool-bar-mode -1)
@@ -143,21 +147,26 @@ current buffer's, reload dir-locals."
                 (when (equal default-directory dir))
                 (my-reload-dir-locals-for-current-buffer)))))
 
-(add-hook 'org-mode-hook 'font-lock-mode)
+;  (add-hook 'org-mode-hook 'font-lock-mode)
 
 (require 'org-tempo)
 
 (add-to-list 'org-modules 'org-tempo t)
-(use-package org-bullets
- :ensure t
- :config
- (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
- (setq org-ellipsis "⮟")
+  (use-package org-bullets
+   :ensure t
+   :config
+   (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
+   (setq org-ellipsis "⮟")
 
-; (setq org-src-tab-acts-natively t)
- (setq org-src-window-setup 'current-window)
- (add-to-list 'org-structure-template-alist
- '("el" . "src emacs-lisp"))
+(font-lock-add-keywords
+ 'org-mode
+ '(("^[[:space:]]*\\(-\\) "
+        (0 (prog1 () (compose-region (match-beginning 1) (match-end 1) "•"))))))
+
+  ; (setq org-src-tab-acts-natively t)
+   (setq org-src-window-setup 'current-window)
+   (add-to-list 'org-structure-template-alist
+   '("el" . "src emacs-lisp"))
 
 (require 'org-tempo)
 
@@ -224,30 +233,31 @@ current buffer's, reload dir-locals."
  (setq TeX-save-query t)
  (setq-default TeX-master nil)
  (setq TeX-PDF-mode t)
-
-(add-hook 'LateX-mode-hook (lambda () (latex-preview-pane-mode)))
-
-(global-set-key (kbd "C-x l ") 'latex-preview-pane-mode)
-
+; (add-hook 'LateX-mode-hook (lambda () (latex-preview-pane-mode)))
+; (global-set-key (kbd "C-x l ") 'latex-preview-pane-mode)
+ (global-set-key (kbd "C-x l ") 'pdflatex)
 (add-to-list 'org-latex-packages-alist '("" "listings" nil))
 (setq org-latex-listings t)   
 (setq org-latex-listings-options '(("breaklines" "true")))
 
 (use-package auctex
-   :hook ((latex-mode LaTeX-mode) . lsp)
-   :config
-  (add-to-list 'font-latex-math-environments "dmath"))
-(use-package auctex-latexmk
-   :after auctex
-   :init
-  (auctex-latexmk-setup))
+      :hook ((latex-mode LaTeX-mode) . tex)
+      :config
+   (add-to-list 'font-latex-math-environments "dmath"))
+;  (use-package auctex-latexmk
+;      :after auctex
+;      :init
+;   (auctex-latexmk-setup))
 
-(add-hook 'LaTeX-mode-hook 'visual-line-mode)
-(add-hook 'LaTeX-mode-hook 'flyspell-mode)
-(add-hook 'LaTeX-mode-hook 'LaTeX-math-mode)
+    (add-hook 'LaTeX-mode-hook 'visual-line-mode)
+    (add-hook 'LaTeX-mode-hook 'flyspell-mode)
+    (add-hook 'LaTeX-mode-hook 'LaTeX-math-mode)
 
-(add-hook 'LaTeX-mode-hook 'turn-on-reftex)
-(setq reftex-plug-into-AUCTeX t)
+    (add-hook 'LaTeX-mode-hook 'turn-on-reftex)
+    (setq reftex-plug-into-AUCTeX t)
+
+;; with latex i like colors :)
+(add-hook 'LaTeX-mode-hook 'font-lock-mode)
 
 (require 'evil)
 (evil-mode 1)
@@ -259,10 +269,14 @@ current buffer's, reload dir-locals."
    (global-set-key (kbd "C-x t") 'multi-term)))
  (setq multi-term-program "/bin/bash")
 
+(ac-config-default)
+
 (use-package yasnippet
       :ensure t
       :init
       (yas-global-mode 1))
+
+
 
 (use-package swiper
       :ensure t
@@ -306,5 +320,8 @@ current buffer's, reload dir-locals."
   :config (setq real-auto-save-interval 10)
   :hook (prog-mode . real-auto-save-mode))
 
-(require 'elcord)
-(elcord-mode)
+(use-package elcord
+:config
+(setq elcord-refresh-rate 5)
+:init
+(elcord-mode))
