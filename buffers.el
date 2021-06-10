5323f7356269587014b2d9cd93b8e39f08fe48f4
jefter66 <jeftersantiago@protonmail.com>
2021-06-09 23:35:13 -0300

    buffers

---
+++ buffers.el
@@ -0,0 +1,19 @@
+
+; Ace Window
+; Controlling buffers
+(use-package ace-window
+  :ensure t
+  :init
+  (progn
+    (global-set-key [remap other-window] 'ace-window)
+    (custom-set-faces
+     '(aw-leading-char-face
+       ((t (:inherit ace-jump-face-foreground :height 2.0)))))))
+
+; Terminals
+(use-package multi-term 
+  :ensure t
+  :config (setq multi-term-program "/bin/bash")
+  (progn
+	(global-set-key (kbd "C-x t") 'multi-term)))
+
