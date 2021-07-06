d8239ceda97383c166b35b7f72cc2e38fa99a812
jefter66 <jeftersantiago@protonmail.com>
2021-07-06 18:12:49 -0300

    back to normal config file

--- init.el
+++ init.el
@@ -1,15 +1,9 @@
-(setq-default user-full-name "Jefter Santiago")
-(setq-default user-mail-address "jeftersantiago@protonmail.com")
-
-(load "~/.local/bin/private.el")
-
-
 (require 'package)
 (setq package-enable-at-startup nil)
-(add-to-list 'package-archives
-	     '("melpa"."https://melpa.org/packages/"))
+(add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/") t)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
 (package-initialize)
-
 
 `use-package'
 (unless(package-installed-p 'use-package)
@@ -19,13 +13,6 @@
 ; puts the outputs in a separated file
 (setq custom-file "~/.emacs.d/custom.el")
 
-(load "~/.emacs.d/env.el")
-(load "~/.emacs.d/programming.el")
-(load "~/.emacs.d/external.el")
-(load "~/.emacs.d/dired.el")
-(load "~/.emacs.d/org.el")
-(load "~/.emacs.d/buffers.el")
-(load "~/.emacs.d/latex.el")
-(load "~/.emacs.d/autocompletation.el")
 
+(org-babel-load-file (expand-file-name "~/.emacs.d/Emacs.org"))
 ;(set-frame-parameter nil 'fullscreen 'fullboth)
