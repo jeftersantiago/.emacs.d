d048cd00137043f9482e64360eb66ea212fdff12
jeftersantiago <jeftersantiago@protonmail.com>
2022-02-05 17:56:27 -0300

    formating the cofig file

--- config.el
+++ config.el
@@ -135,13 +135,13 @@
 ;  (add-to-list 'load-path "~/.emacs.d/evil")
 ;  (require 'evil)
 
- (use-package evil
-   :ensure t
-   :init
-   (setq evil-want-integration t) ;; This is optional since it's already set to t by default.
-   (setq evil-want-keybinding nil)
-   :config
-   (evil-mode 1))
+;(use-package evil
+;  :ensure t
+;  :init
+;  (setq evil-want-integration t) ;; This is optional since it's already set to t by default.
+;  (setq evil-want-keybinding nil)
+;  :config
+;  (evil-mode 1))
 
 ; (use-package evil-collection
 ;   :after evil
